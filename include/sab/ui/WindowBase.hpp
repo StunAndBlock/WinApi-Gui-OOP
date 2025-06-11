2ee890852a70d57214917198a65d9753305a1bfe
StunAndBlock <dword.byteme@mail.ru>
2025-06-11 22:01:00 +0300

    refactor: All the API is now located in include folder to match proper include style: include <name_of_external_headers_dir>/<dev_name>/<partition>/<header_name> Source code is now located in src folder and has same folder hierarchy as include excpet the dev_name is removed. GuiBase class renamed into WindowBase and now mean to be responsible only for Window creation hMain_ field renamed into hWnd_ because one window class stands for one window handler and it cant be anymore. Handler method no longer maintained be __stdcall, now maintained as it should be by default __pthiscall Handler no longer has HWND hwnd in parameter signature, due to duplication of hwnd (hwnd and hWnd_ are the same). performance: New InitProc window procedure shoud be now used as initial value of wc.lpfnWndProc. It allows to link "this" pointer as GLWP_USERDATA to hWnd_ once without checking condition msg == WM_NCCREATE each time procedure is called.

---
+++ include/sab/ui/WindowBase.hpp
@@ -0,0 +1,32 @@
+#ifndef SAB_WINUI_WINDOWBASE_HPP_
+#define SAB_WINUI_WINDOWBASE_HPP_
+#include <Windows.h>
+
+
+#ifdef SAB_WINUI_LIB_EXPORT
+#define SAB_WINUI_LIB __declspec(dllexport)
+#else
+#define SAB_WINUI_LIB __declspec(dllimport)
+#endif
+
+namespace sab {
+    namespace winui {
+
+        class SAB_WINUI_LIB WindowBase{
+            protected:
+                HWND hWnd_;
+                virtual LRESULT Handler(UINT ,WPARAM, LPARAM );
+                static LRESULT __stdcall InitProc(HWND, UINT, WPARAM, LPARAM);
+                static LRESULT __stdcall WindowProc(HWND, UINT, WPARAM , LPARAM);
+            public:
+                WindowBase();
+                virtual ~WindowBase() = default;
+                virtual void Show();
+                virtual LRESULT SetupWin(RECT, HWND) = 0;
+        };
+
+    };
+};
+
+
+#endif //!SAB_UI_WINDOWBASE
